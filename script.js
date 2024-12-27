43180cd486c9650400f3ec08261ab0064f7bd5f8
sSsamirRr-mine <sssamirrr4@gmail.com>
2024-12-27 16:13:18 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+'use strict';
+
+// let ism = prompt("Ismingizni yozing")
+
+// let famil = prompt("Familiya")
+
+// let Yosh = prompt("Yosh")
+
+// let kim = prompt("Kimni yaxshi ko'rasiz")
+
+// let oyin = prompt("yaxshi ko'rgan o'yin")
+
+// let arr = []
+
+// arr.push(ism)
+
+// arr.push(famil)
+
+// if (Yosh > 18) {
+//     arr.push("Siz 18 Yoshdan kattaroqsiz")
+// } else if (Yosh == 18) {
+//     arr.push("Siz 18 Yoshga tengsiz")
+// } else { 
+//     arr.push("Siz 18 Yoshdan kichik siz")
+// }
+
+// arr.push(kim)
+
+// arr.push(oyin)
+
+// let body = document.querySelector("body")
+
+// let div = document.createElement("div")
+
+// arr.forEach(item => {
+
+//     let h2 = document.createElement("h2")
+
+//     let br = document.createElement("br")
+
+//     h2.innerHTML = item
+
+//     div.appendChild(h2)
+
+//     div.appendChild(br)
+
+// });
+
+// body.appendChild(div)
